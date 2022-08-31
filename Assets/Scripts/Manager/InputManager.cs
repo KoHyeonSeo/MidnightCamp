e3a7acba38b8ce98e5700a8a20528cc3745b0d38
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-08-31 17:29:41 +0900

    Update InputManager.cs

--- Assets/Scripts/Manager/InputManager.cs
+++ Assets/Scripts/Manager/InputManager.cs
@@ -1,11 +1,20 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using System;
 
+[Serializable]
+public struct BlockInfo
+{
+    public Vector3 position;
+    public Vector3 rotation;
+    public Vector3 scale;
+}
 public class InputManager : MonoBehaviour
 {
     public const string MouseLeftClickName = "Fire1";
     public const string MouseRightClickName = "Fire2";
+    public const string MouseWheelClickName = "Fire3";
     public const string MouseXoutName = "Mouse X";
     public const string MouseYoutName = "Mouse Y";
     public const string MouseScrollName = "Mouse ScrollWheel";
@@ -22,6 +31,10 @@ public class InputManager : MonoBehaviour
     /// Mouse 오른쪽 버튼을 클릭하고 있을 시 true 반환 (GetButton)
     /// </summary>
     public bool MouseRightClick { get; private set; }
+    /// <summary>
+    /// 마우스 Wheel Click시 true 값 반환
+    /// </summary>
+    public bool MouseWheelClick { get; private set; }
     /// <summary>
     /// Mouse의 X좌표 움직임 값 반환
     /// </summary>
@@ -52,13 +65,23 @@ public class InputManager : MonoBehaviour
     /// Mouse Pointer가 가리키는 블록의 hit Point를 반환 
     /// </summary>
     public Vector3 ObjectHitPoint { get; private set; }
+    /// <summary>
+    /// Mouse Pointer가 가리키는 Object의 정보를 반환 
+    /// </summary>
+    public BlockInfo PointBlockInformation { get; private set; }
     #endregion
 
     private void Update()
     {
+        //if(ObjectHitPoint != Vector3.zero)
+        //{
+        //    Debug.Log($"ObjectHitPoint: {ObjectHitPoint}");
+        //}
+        //Debug.Log($"Position: {PointBlockInformation.position}\nRotation: {PointBlockInformation.rotation}\nScale: {PointBlockInformation.scale}");
         #region 마우스 관련 Input
         MouseLeftClick = Input.GetButton(MouseLeftClickName);
         MouseRightClick = Input.GetButton(MouseRightClickName);
+        MouseWheelClick = Input.GetButton(MouseWheelClickName);
         MouseXOut = Input.GetAxis(MouseXoutName);
         MouseYOut = Input.GetAxis(MouseYoutName);
         MouseWheelScroll = Input.GetAxis(MouseScrollName);
@@ -74,7 +97,17 @@ public class InputManager : MonoBehaviour
         Debug.DrawRay(screenPoint, dir.normalized * mouseZMaxDistance, Color.red);
         if (Physics.Raycast(screenPoint, dir.normalized * mouseZMaxDistance, out hit, mouseZMaxDistance))
         {
+            //가리키는 Object 담기
             PointBlock = hit.collider.gameObject;
+
+            //구조체로 정보 담기
+            BlockInfo block;
+            block.position = PointBlock.gameObject.transform.position;
+            block.rotation = PointBlock.gameObject.transform.rotation.eulerAngles;
+            block.scale = PointBlock.gameObject.transform.localScale;
+            PointBlockInformation = block;
+            
+            //hitPoint 전달
             ObjectHitPoint = hit.point;
         }
         else
