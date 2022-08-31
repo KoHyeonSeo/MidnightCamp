e58a4a6dae749d1dc98f20074df87a27098be43f
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-09-01 00:33:26 +0900

    Update GameManager.cs

--- Assets/Scripts/Manager/GameManager.cs
+++ Assets/Scripts/Manager/GameManager.cs
@@ -28,10 +28,73 @@ public class GameManager : MonoBehaviour
         Delete,
         ColorChange
     }
-
     /// <summary>
     /// 현재 모드
     /// </summary>
     public Mode curState = Mode.Move;
 
+    [SerializeField] private GameObject system;
+
+    private DragBlock movingScript;
+    private DeleteMode deleteScript;
+    private RotationBlock rotateScript;
+
+    private Mode preState = Mode.Move;
+
+    private void Start()
+    {
+        movingScript = system.GetComponent<DragBlock>();
+        deleteScript = system.GetComponent<DeleteMode>();
+        rotateScript = system.GetComponent<RotationBlock>();
+        ChangeState();
+    }
+
+    private void Update()
+    {
+        if(preState != curState)
+        {
+            preState = curState;
+            ChangeState();
+        }
+    }
+    void ChangeState()
+    {
+        switch (curState)
+        {
+            case Mode.Move:
+                Move();
+                break;
+            case Mode.Rotate:
+                Rotate();
+                break;
+            case Mode.Delete:
+                Delete();
+                break;
+            case Mode.ColorChange:
+                ColorChage();
+                break;
+        }
+    }
+    void Move()
+    {
+        movingScript.enabled = true;
+        rotateScript.enabled = false;
+        deleteScript.enabled = false;
+    }
+    void Rotate()
+    {
+        rotateScript.enabled = true;
+        movingScript.enabled = false;
+        deleteScript.enabled = false;
+    }
+    void Delete()
+    {
+        deleteScript.enabled = true;
+        movingScript.enabled = false;
+        rotateScript.enabled = false;
+    }
+    void ColorChage()
+    {
+
+    }
 }
