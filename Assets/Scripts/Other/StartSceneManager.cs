608c292c3c255fb521f37c72383a2bcdd4d0b8c4
kwonyoungchan <kyckyc0530@gmail.com>
2022-09-01 21:05:19 +0900

    add_StartScene

--- Assets/Scripts/Other/StartSceneManager.cs
+++ Assets/Scripts/Other/StartSceneManager.cs
@@ -1,27 +1,69 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class StartSceneManager : MonoBehaviour
 {
     // Start is called before the first frame update
     float toLerp;
-    [SerializeField]float h;
+    [SerializeField] float h;
+    [SerializeField] ParticleSystem spark;
+    [SerializeField] Transform Head;
+    ParticleSystem Spark;
+    AudioSource audio;
+    [SerializeField] Light light;
+    [SerializeField] float intensityMax;
+    [SerializeField] float intensitySpeed;
+    [SerializeField] float setTime;
+    float waitTime;
+    bool isHit;
     void Start()
     {
+        Spark=Instantiate(spark);
+        audio = GetComponent<AudioSource>();
+        
         
     }
 
     // Update is called once per frame
     void Update()
     {
+        #region 여기는  망치가 치는 모션 
         if (toLerp < 120)
         {
-            toLerp = Mathf.Lerp(toLerp, h, 3* Time.deltaTime);
+            toLerp = Mathf.Lerp(toLerp, h,  4*Time.fixedDeltaTime);
             transform.rotation = Quaternion.Euler(0, 0, toLerp);
-            print(toLerp);
         }
-        print(transform.rotation.eulerAngles);
+        #endregion
+        #region 여기는 조명 밝아지는 모션
+        if (isHit)
+        {
+            if (light.intensity >= intensityMax)
+            {
+                light.intensity = intensityMax;
+                waitTime += Time.deltaTime;
+            }
+            light.intensity += intensitySpeed;
+        }
+        #endregion
+//================================================여기서부터 씬 전환하는 부분
+        if (waitTime > setTime)
+        {
+            print("씬 전환");
+            // 여기서 씬 로드
+            //SceneManager.LoadScene("");
+        }
         
     }
+    private void OnCollisionEnter(Collision collision)
+    {
+        print("닿음");
+        Spark.transform.position = Head.position;
+        Spark.Play();
+        audio.Play();
+        isHit = true;
+    }
+
+
 }
