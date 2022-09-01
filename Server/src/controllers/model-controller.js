82c759c2cdb5801713f43f3b631cc0e3c5bb8d26
kicpark <pkch0102@gmail.com>
2022-09-01 10:48:46 +0900

    /model 라우터 생성및 기능 구현
    select 구현

--- Server/src/controllers/model-controller.js
+++ Server/src/controllers/model-controller.js
@@ -13,8 +13,31 @@ exports.searchAllModels = async (req, res, next) => {
 }
 
 exports.searchModels = async (req, res, next) => {
-    const data = req.query
+    const data = req.query;
+    console.log(data);
     const results = await ModelService.searchModels(data);
+
+    res.status(HttpStatus.OK).json({
+        status: HttpStatus.OK,
+        message: 'successfully found all Models',
+        results: results
+    });
+}
+
+
+exports.searchDetailByCode = async (req, res, next) => {
+    const code = req.params;
+    const results = await ModelService.searchDetailByCode(code);
+
+    res.status(HttpStatus.OK).json({
+        status: HttpStatus.OK,
+        message: 'successfully found all Models',
+        results: results
+    });
+}
+
+exports.insertModel = async (req, res, next) => {
+    
 
     res.status(HttpStatus.OK).json({
         status: HttpStatus.OK,
