5a76855db2a26f3c8730762354611d199f51f56d
KoHyeonSeo <76097749+KoHyeonSeo@users.noreply.github.com>
2022-09-02 06:08:23 +0900

    Add AR Setting

--- Assets/Scripts/AR/ARCameraTest.cs
+++ Assets/Scripts/AR/ARCameraTest.cs
@@ -11,11 +11,17 @@ public class ARCameraTest : MonoBehaviour
     // Start is called before the first frame update
     void Start()
     {
-        
+        ProjectManager.instance.OnClickLoad();
+        //SetPosition();
     }
 
     // Update is called once per frame
     void FixedUpdate()
+    {
+        //root.transform.position = arCamera.transform.position + arCamera.transform.forward * distance;
+    }
+
+    public void SetPosition()
     {
         root.transform.position = arCamera.transform.position + arCamera.transform.forward * distance;
     }
